7b6c765b05210524bc1cff5021d4e58c93231495
Ingvarrr <falcon.only@gmail.com>
2016-10-17 15:48:28 +0300

    1234

--- Hometask/src/Main.java
+++ Hometask/src/Main.java
@@ -1,7 +1,16 @@
-package PACKAGE_NAME;
-
 /**
  * Created by vig on 10/7/16.
  */
 public class Main {
+    public static void main(String[] args) {
+        Plane su27 = new Plane();
+        su27.setName("SU-27");
+        su27.setPurpose("Military");
+        su27.setSpeed("1400");
+        Plane an178 = new Plane();
+        an178.setName("AN-178");
+        an178.setPurpose("Civilian");
+        an178.setSpeed("825");
+        System.out.println("AN-178 Top speed" +" " + "=" + " " + an178.getSpeed());
+    }
 }
--- Hometask/src/Plane.java
+++ Hometask/src/Plane.java
@@ -1,19 +1,7 @@
 /**
  * Created by vig on 10/6/16.
  */
-public class HT_Plane {
-    public static void main(String[] args) {
-        HT_Plane su27 = new HT_Plane();
-        su27.setName("SU-27");
-        su27.setPurpose("Military");
-        su27.setSpeed("1400");
-        HT_Plane an178 = new HT_Plane();
-        an178.setName("AN-178");
-        an178.setPurpose("Civilian");
-        an178.setSpeed("825");
-        System.out.println(an178.getSpeed());
-
-    }
+public class Plane {
     private String name;
     private String purpose;
     private String speed;
